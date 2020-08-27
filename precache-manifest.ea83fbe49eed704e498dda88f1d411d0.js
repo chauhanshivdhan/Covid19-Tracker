c2cb904eb4cad9e21989d36672de6de4d8df835b
chauhanshivdhan <chauhanshivdhan1999@gmail.com>
2020-08-27 23:32:01 +0530

    Updates

---
+++ precache-manifest.ea83fbe49eed704e498dda88f1d411d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d203e149fe3cd3b90ac32b94f6dd99",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "08b0d704dbc09740c65a",
+    "url": "/Covid19-Tracker/static/css/main.b326ff21.chunk.css"
+  },
+  {
+    "revision": "af6a1eb060e3498f92d1",
+    "url": "/Covid19-Tracker/static/js/2.117b5372.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid19-Tracker/static/js/2.117b5372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b0d704dbc09740c65a",
+    "url": "/Covid19-Tracker/static/js/main.f76762f7.chunk.js"
+  },
+  {
+    "revision": "58dd2541b5ff91463868",
+    "url": "/Covid19-Tracker/static/js/runtime-main.b658d57a.js"
+  },
+  {
+    "revision": "32f944ee080c4fdcabee404705dc3757",
+    "url": "/Covid19-Tracker/static/media/logo.32f944ee.png"
+  }
+]);
